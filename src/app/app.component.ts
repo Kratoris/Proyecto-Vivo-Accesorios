412442e21ba9b712b72916854c72825263cd12fa
kratoris <fegm8042@gmail.com>
2023-05-20 17:04:02 -0500

    carga inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'pruebaCheckBox';
+export class AppComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService
+      .getProducts()
+      .subscribe((products) => (this.products = products));
+  }
+
+  //checkAllCheckBox(ev) { // Angular 9
+  checkAllCheckBox(ev: any) {
+    // Angular 13
+    this.products.forEach((x) => (x.checked = ev.target.checked));
+  }
+
+  isAllCheckBoxChecked() {
+    return this.products.every((p) => p.checked);
+  }
 }
